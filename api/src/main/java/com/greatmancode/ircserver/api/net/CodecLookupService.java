c4d51c5040cd7c241b9a87ba5a9f2324ca914edb
William Lebel <lebelwilliam@gmail.com>
2013-10-20 00:39:50 -0400

    Fix headers everywhere.

--- api/src/main/java/com/greatmancode/ircserver/api/net/CodecLookupService.java
+++ api/src/main/java/com/greatmancode/ircserver/api/net/CodecLookupService.java
@@ -1,20 +1,20 @@
 /*
- * This file is part of ${name}.
+ * This file is part of IRCServer API.
  *
- * ${copyright} <http://www.greatmancode.com/>
+ * Copyright (c) 2013-2013, Greatmancode <http://www.greatmancode.com/>
  *
- * ${name} is free software: you can redistribute it and/or modify
+ * IRCServer API is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * ${name} is distributed in the hope that it will be useful,
+ * IRCServer API is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ${name}.  If not, see <http://www.gnu.org/licenses/>.
+ * along with IRCServer API.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.greatmancode.ircserver.api.net;
 
